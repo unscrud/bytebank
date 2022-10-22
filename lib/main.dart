1665e96829bcc31f158248113f4bb5f3ff9eeca8
Daniel Carvalho <daniel.devop@gmail.com>
2022-10-22 16:19:11 -0300

    montagem de um ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,31 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          body: Column(
+            children: const <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text("200.0"),
+                  subtitle: Text("3000.0"),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text("200.0"),
+                  subtitle: Text("1000.0"),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text("200.0"),
+                  subtitle: Text("2000.0"),
+                ),
+              ),
+            ],
+          ),
           appBar: AppBar(
             title: const Text("Transferências"),
           ),
