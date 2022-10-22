322df15a9f924f00eedf37dc048aa60f54cb97a8
Daniel Carvalho <daniel.devop@gmail.com>
2022-10-22 12:04:34 -0300

    primeiro exemplo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp();
+void main() => runApp(
+      Column(
+        children: const <Widget>[
+          Text(
+            "TEXTO 1",
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            "TEXTO 2",
+            textDirection: TextDirection.rtl,
+          ),
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: FlutterLogo(),
+            ),
+          ),
+        ],
+      ),
+    );
