4773284cba8621d11ecec7613ed287dd5f5f303e
Daniel Carvalho <daniel.devop@gmail.com>
2022-10-22 12:29:33 -0300

    material app com appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      Column(
-        children: const <Widget>[
-          Text(
-            "TEXTO 1",
-            textDirection: TextDirection.ltr,
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Transferências"),
           ),
-          Text(
-            "TEXTO 2",
-            textDirection: TextDirection.rtl,
-          ),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: FlutterLogo(),
-            ),
-          ),
-        ],
+        ),
       ),
     );
