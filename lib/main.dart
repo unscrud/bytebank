6b27862508d0af15080d61d50569b0c0bb1928c5
Daniel Carvalho <daniel.devop@gmail.com>
2022-10-22 16:21:34 -0300

    Separando a lista em uma classe

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,43 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          body: Column(
-            children: const <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text("200.0"),
-                  subtitle: Text("3000.0"),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text("200.0"),
-                  subtitle: Text("1000.0"),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text("200.0"),
-                  subtitle: Text("2000.0"),
-                ),
-              ),
-            ],
-          ),
+          body: const ListaTransferencia(),
           appBar: AppBar(
             title: const Text("Transferências"),
           ),
         ),
       ),
     );
+
+class ListaTransferencia extends StatelessWidget {
+  const ListaTransferencia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text("200.0"),
+            subtitle: Text("3000.0"),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text("200.0"),
+            subtitle: Text("1000.0"),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text("200.0"),
+            subtitle: Text("2000.0"),
+          ),
+        ),
+      ],
+    );
+  }
+}
