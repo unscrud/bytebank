31050deed712f6b4eedadc5b46d16773923ef12c
Daniel Carvalho <daniel.devop@gmail.com>
2022-11-02 06:30:07 -0300

    item da lista

---
+++ lib/screens/transferencia/item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../../models/transferencia.dart';
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
